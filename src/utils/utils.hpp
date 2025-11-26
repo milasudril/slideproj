508f36425667975eefc76ff2927bb0b8bbc8a2c2
Torbjörn Rathsman <milasudril@gmail.com>
2025-11-26 21:57:26 +0100

    Add variant factory

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -7,6 +7,9 @@
 #include <string_view>
 #include <charconv>
 #include <algorithm>
+#include <array>
+#include <cstddef>
+#include <variant>
 
 namespace slideproj::utils
 {
@@ -70,5 +73,52 @@ namespace slideproj::utils
 	template<bitmask_enum T>
 	constexpr auto is_set(T a, T value)
 	{ return static_cast<bool>(a & value); }
+
+	template<class T>
+	struct make_variant_type_tag
+	{ using type = T; };
+
+	template<size_t Index, class VariantType, class Factory>
+	consteval void fill_make_variant_vtable(
+		std::array<VariantType (*)(Factory&&), std::variant_size_v<VariantType>>& vtable
+	)
+	{
+		if constexpr(Index == std::variant_size_v<VariantType>)
+		{ return; }
+		else
+		{
+			using type = std::variant_alternative_t<Index, VariantType>;
+			using type_tag =  make_variant_type_tag<type>;
+			static_assert(
+				std::is_same_v<std::invoke_result_t<Factory, type_tag>, type>,
+				"Factory returns wrong type"
+			);
+			vtable[Index] = [](Factory&& f) {
+				return VariantType(std::forward<Factory>(f)(make_variant_type_tag<type>{}));
+			};
+			return fill_make_variant_vtable<Index + 1>(vtable);
+		}
+	}
+
+	template<class VariantType, class Factory>
+	consteval auto create_make_variant_vtable()
+	{
+			std::array<VariantType (*)(Factory&&), std::variant_size_v<VariantType>> ret{};
+			fill_make_variant_vtable<0>(ret);
+			return ret;
+	}
+
+	template<class VariantType, class Factory>
+	constexpr auto make_variant_vtable = create_make_variant_vtable<VariantType, Factory>();
+
+	template<class VariantType, class Factory>
+	constexpr auto make_variant(size_t kind, Factory&& factory)
+	{
+		constexpr auto& vtable = make_variant_vtable<VariantType, Factory>;
+		if(kind >= std::size(vtable)) [[unlikely]]
+		{ return VariantType{}; }
+
+		return vtable[kind](std::forward<Factory>(factory));
+	}
 }
 #endif
