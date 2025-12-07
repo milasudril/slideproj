fa90398a1d695391378be18234f4f11fb7a7b640
Torbjörn Rathsman <milasudril@gmail.com>
2025-12-07 17:29:57 +0100

    Add a bidirectional_sliding_window

---
+++ src/utils/bidirectional_sliding_window.hpp
@@ -0,0 +1,54 @@
+#ifndef SLIDEPROJ_UTILS_BIDIRECTIONAL_SLIDING_WINDOW_HPP
+#define SLIDEPROJ_UTILS_BIDIRECTIONAL_SLIDING_WINDOW_HPP
+
+#include <array>
+#include <utility>
+#include <cstddef>
+#include <limits>
+
+namespace slideproj::utils
+{
+	template<class T, size_t Radius>
+	class bidirectional_sliding_window
+	{
+	public:
+		static constexpr auto elem_count = 2*Radius + 1;
+
+		explicit bidirectional_sliding_window(std::array<T, elem_count>&& vals):
+			m_values{std::move(vals)}
+		{}
+
+		void step_forward()
+		{
+			m_index_to_replace = (elem_count + m_current_index - Radius)%elem_count;
+			m_current_index = (m_current_index + 1)%elem_count;
+		}
+
+		void step_backward()
+		{
+			m_index_to_replace = (m_current_index + Radius)%elem_count;
+			m_current_index = (elem_count + m_current_index - 1)%elem_count;
+		}
+
+		T const& get_current_element() const
+		{ return m_values[m_current_index]; }
+
+		// TODO: C++26 optional reference
+		T* get_element_to_replace()
+		{
+			if(m_index_to_replace == std::numeric_limits<size_t>::max())
+			{ return nullptr; }
+
+			return &m_values[m_index_to_replace];
+		}
+
+		auto const& get_values() const
+		{ return m_values; }
+
+	private:
+		std::array<T, elem_count> m_values;
+		size_t m_current_index = elem_count/2;
+		size_t m_index_to_replace = std::numeric_limits<size_t>::max();
+	};
+}
+#endif
