fb3a7fbc4f3248862a3973dbd2844455c24c2880
Torbjörn Rathsman <milasudril@gmail.com>
2025-12-12 18:24:38 +0100

    Update slideshow_controller

--- src/app/slideshow_controller.hpp
+++ src/app/slideshow_controller.hpp
@@ -34,52 +34,45 @@ namespace slideproj::app
 			if(image_to_show != nullptr)
 			{ fprintf(stderr, "(i) Showing %s\n", image_to_show->path().c_str()); }
 		}
-#if 0
+
 		void start_slideshow(std::reference_wrapper<slideshow> slideshow)
 		{
 			fprintf(stderr, "(i) Slideshow loaded\n");
 			m_current_slideshow = &slideshow.get();
 			unwrap(m_task_queue).submit(
-				[
-					images_to_load = std::array{
-						m_current_slideshow->get_entry(-1),
-						m_current_slideshow->get_entry(0),
-						m_current_slideshow->get_entry(1)
-					},
-					rect = m_target_rectangle,
-					worker_result = std::ref(m_worker_result),
-					loaded_images  = std::ref(m_loaded_images)
-				](){
-					std::array<pixel_store::rgba_image, std::tuple_size_v<decltype(images_to_load)>> img_array;
-					for(size_t k = 0; k != std::size(images_to_load); ++k)
-					{
-						if(images_to_load[k] != nullptr)
+				utils::task{
+					.function = [
+							images_to_load = std::array{
+								m_current_slideshow->get_entry(-1),
+								m_current_slideshow->get_entry(0),
+								m_current_slideshow->get_entry(1)
+							},
+							rect = m_target_rectangle
+					](){
+						std::array<pixel_store::rgba_image, std::tuple_size_v<decltype(images_to_load)>> img_array;
+						for(size_t k = 0; k != std::size(images_to_load); ++k)
 						{
-							auto const& path_to_load = images_to_load[k]->path();
-							fprintf(stderr, "(i) Loading %s\n", path_to_load.c_str());
-							img_array[k]  = image_file_loader::load_rgba_image(path_to_load, rect);
+							if(images_to_load[k] != nullptr)
+							{
+								auto const& path_to_load = images_to_load[k]->path();
+								fprintf(stderr, "(i) Loading %s\n", path_to_load.c_str());
+								img_array[k]  = image_file_loader::load_rgba_image(path_to_load, rect);
+							}
 						}
-					}
-					utils::unwrap(worker_result) = [
-						img_array = std::move(img_array),
-						loaded_images
-					]() mutable {
+						return std::remove_cvref_t<decltype(m_loaded_images)>{std::move(img_array)};
+					},
+					.on_completed = [loaded_images  = std::ref(m_loaded_images)](auto&& img_array) mutable {
 						fprintf(stderr, "(i) First images loaded\n");
-						auto& pending_images = utils::unwrap(loaded_images);
-						pending_images = typename decltype(loaded_images)::type{
-							std::move(img_array)
-						};
-					};
+						utils::unwrap(loaded_images) = std::move(img_array);
+					}
 				}
 			);
 		}
-#endif
 
 	private:
 		std::reference_wrapper<utils::task_queue> m_task_queue;
 		slideshow* m_current_slideshow{nullptr};
 		image_file_loader::image_rectangle m_target_rectangle{};
-		// TODO: This needs to be a threadsafe queue
 		utils::synchronized<std::move_only_function<void()>> m_worker_result;
 		utils::bidirectional_sliding_window<pixel_store::rgba_image, 1> m_loaded_images;
 		bool m_application_should_exit{false};
