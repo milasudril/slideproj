0bd4ca34e7c070819844a18ab98af031186a9c17
Torbjörn Rathsman <milasudril@gmail.com>
2025-12-14 12:27:36 +0100

    Add .jpg back to input filter

--- src/app/slideproj.cpp
+++ src/app/slideproj.cpp
@@ -53,6 +53,7 @@ int main()
 					"/home/torbjorr/Bilder",
 					slideproj::app::input_filter{
 						.include = std::vector{
+							slideproj::app::input_filter_pattern{"*.jpg"},
 							slideproj::app::input_filter_pattern{"*.jpeg"},
 							slideproj::app::input_filter_pattern{"*.bmp"},
 							slideproj::app::input_filter_pattern{"*.gif"},
